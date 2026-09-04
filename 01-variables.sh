b029faadf883a7c7ebbe2c6362650bb51669d5e1
pakalamandasagar <pakalamandasagar955@gmail.com>
2026-09-04 11:43:59 +0530

    shell -practice

--- 01-variables.sh
+++ 01-variables.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-PERSON1=Trump
-PERSON2=Musk
+TRUMP=Trump
+MUSK=Musk
 
 echo "Trump:: Hey Musk, how are you?"
 echo "Musk:: Hello Trump, I am fine. How are you doing"
 echo "Trump:: I am fine too. what's up?"
 echo "Musk:: Nothing, just going to Mars now, will yot come?"
 echo "Trump:: Sorry you carry on! I will come once you come back"
+
---
+++ 02-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$PERSON1:: Hey $PERSON2, how are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing"
+echo "$PERSON1:: I am fine too. what's up?"
+echo "$PERSON2:: Nothing, just going to Mars now, will yot come?"
+echo "$PERSON1:: Sorry you carry on! I will come once you come back"
