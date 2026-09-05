be5f11e71a08de7f36074eae2dc8cded6d701835
pakalamandasagar <pakalamandasagar955@gmail.com>
2026-09-05 11:50:22 +0530

    Shell-Practice

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -10,12 +10,29 @@ else
     echo "You are running with root access"
 fi
 
-dnf install mysql -y
+dnf list installed Mysql 
 
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
 then
-    echo "Installing MySQL is ...SUCCESS"
-else
-    echo "Installing MySQL is ...FAILURE"
-    exit 1 #ERROR vacina prati sari exit status ivvali
+    echo "MySQL is not installed... going to install it"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "Installing MySQL is ...SUCCESS"
+    else
+        echo "Installing MySQL is ...FAILURE"
+        exit 1 #ERROR vacina prati sari exit status ivvali
+    fi
+else 
+    echo "MySQL is already installed... Nothing to do"
 fi
+
+# dnf install mysql -y
+
+# if [ $? -eq 0 ]
+# then
+#     echo "Installing MySQL is ...SUCCESS"
+# else
+#     echo "Installing MySQL is ...FAILURE"
+#     exit 1 #ERROR vacina prati sari exit status ivvali
+# fi
