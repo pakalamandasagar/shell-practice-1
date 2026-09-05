6ce4ba7f11d7fc7e5afce9e922c967d505db0e4c
pakalamandasagar <pakalamandasagar955@gmail.com>
2026-09-05 11:22:36 +0530

    Shell-Practice

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -17,5 +17,5 @@ then
     echo "Installing MySQL is ...SUCCESS"
 else
     echo "Installing MySQL is ...FAILURE"
-    #exit 1 #ERROR vacina prati sari exit status ivvali
+    exit 1 #ERROR vacina prati sari exit status ivvali
 fi
