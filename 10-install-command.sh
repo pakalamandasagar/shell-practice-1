3acc302fd375e6f7b0b502af01eee20399f19a61
pakalamandasagar <pakalamandasagar955@gmail.com>
2026-09-05 11:20:32 +0530

    Shell-Practice

---
+++ 10-install-command.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run thid script with root access"
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access"
+fi
+
+dnf install myqulll -y
+
+if [ $? -eq 0 ]
+then
+    echo "Installing MySQL is ...SUCCESS"
+else
+    echo "Installing MySQL is ...FAILURE"
+    #exit 1 #ERROR vacina prati sari exit status ivvali
+fi
